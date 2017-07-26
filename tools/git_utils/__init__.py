31205409ac0232dcf2be7b1ea4319be27dce5120
Hualet Wang <mr.asianwang@gmail.com>
2017-07-26 10:33:31 +0800

    Fix dcr crash on manipulating a single project
    Change-Id: Iab847cf9a0f4765bea035642fd7d8144bb8f0136

--- tools/git_utils/__init__.py
+++ tools/git_utils/__init__.py
@@ -14,7 +14,7 @@ class RepoManager(Repo):
         try:
             sbm = super().submodule(name)
         except ValueError:
-            raise SubmoduleError("This git repository doesn't have this submodule")
+            raise SubmoduleError("This git repository doesn't have this submodule: %s" % (self.name))
         return sbm
 
     @property
@@ -23,28 +23,32 @@ class RepoManager(Repo):
             return self._sbms
         self._sbms = super().submodules
         if not self._sbms:
-            raise SubmoduleError("This git repository doesn't have any submodule")
+            raise SubmoduleError("This git repository doesn't have any submodule: %s" %s (self.name))
         return self._sbms
 
     @property
     def tags(self):
         tags = super().tags
         if not tags:
-            raise TagError("This git repository doesn't have any tag")
+            return None
         return tags
 
     def get_submodule(self, name):
-        repo = RepoManager(self.submodule(name).abspath)
-        repo.name = name
-        return repo
+        sbms = self.get_submodules((name,))
+        return sbms[0] if len(sbms) else None
 
-    def get_all_submodules(self):
+
+    def get_submodules(self, names):
         sbms = []
-        for sbm in self.submodules:
+        for name in names:
+            sbm = self.submodule(name)
             repo = RepoManager(sbm.abspath)
             repo.name = sbm.name
             sbms.append(repo)
         return sbms
+
+    def get_all_submodules(self):
+        return self.get_submodules(self.submodule_list)
 
     @property
     def submodule_list(self):
@@ -60,13 +64,14 @@ class RepoManager(Repo):
 
     @property
     def latest_tag(self):
-        return max(self.tags, key=lambda t: t.commit.committed_datetime)
+        return max(self.tags, key=lambda t: t.commit.committed_datetime) if self.tags else None
 
     def commit_to_tag(self, commit_hash):
-        for tag in self.tags:
-            if commit_hash == tag.commit.hexsha:
-                return tag
-        raise TagError("This commit can't convert into tag")
+        if self.tags:
+            for tag in self.tags:
+                if commit_hash == tag.commit.hexsha:
+                    return tag
+        raise TagError("This commit %s of repo %s can't convert into tag" % (commit_hash, self.name))
 
     def set_commit(self, commit_hash):
         return self.git.checkout(commit_hash)
